e13792c52372f8edfbc061385fd4249afca77238
Myeonghyeon Kim <mhkim100@postech.ac.kr>
2020-11-08 03:08:45 +0900

    create sys_exec and sys_wait

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -33,3 +33,28 @@ void sys_exit(int status)
   printf("%s: exit(%d)\n", thread_current()->name, status);
   thread_exit();
 }
+
+pid_t sys_exec(const char *cmd_line)
+{
+  char *args;
+  pid_t pid;
+  // function parsing cmd_line
+  
+  // thread_create(args[])
+
+  return pid;
+}
+
+int sys_wait (pid_t pid)
+{
+  struct thread *parent;
+  struct thread *child;
+  // add 'child_list' to thread structure
+  if (!(child = thread_get_child(pid)))
+    return -1;
+  
+  sema_down(parent->wait); // add 'wait' semaphore to thread structures  
+  list_remove(child->child_elem); // add 'child_elem'
+
+  return child->status;
+}
