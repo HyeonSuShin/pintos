04b22b5ffd6c6e9cbbdd9f5064bb3c1ed46d714a
seungyeon-seo <kcd6256@gmail.com>
2020-11-08 23:54:17 +0900

    create syscall functions for file

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -57,3 +58,19 @@ int sys_wait (pid_t pid)
   return process_wait(pid);
 }
 
+bool sys_create (const char *file, unsigned initial_size)
+{
+  return filesys_create(file, initial_size);
+}
+
+bool sys_remove (const char *file)
+{
+  return filesys_remove(file);
+}
+
+int sys_open (const char *file)
+{
+  struct file *file;
+  file = filesys_open(file);
+  
+}
