3b735568ab59b2a6cae3c901ce512e5a64ba7b09
seungyeon-seo <kcd6256@gmail.com>
2020-11-09 22:25:34 +0900

    add thread to childlist of parent thread when created

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -622,6 +622,7 @@ init_thread (struct thread *t, const char *name, int priority)
   sema_init(t->load, 0);
   sema_init(t->wait, 0);
   t->next_fd = 2;
+  list_push_back(thread_current()->child_list, &t->child_elem);
 
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
